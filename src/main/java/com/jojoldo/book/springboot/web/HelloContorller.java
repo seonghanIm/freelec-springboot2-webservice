fcb6933c32d49c3cac004daad3e24a7ea8c7fa81
SeongHanIm <tjdgksdl1120@gmail.com>
2022-03-24 17:57:12 +0900

    api 생성 및 test완료

---
+++ src/main/java/com/jojoldo/book/springboot/web/HelloContorller.java
@@ -0,0 +1,10 @@
+package com.jojoldo.book.springboot.web;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloContorller {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
